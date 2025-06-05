3bf3f70500228916ac11d88a133e9715d41987b3
truongnguyen3 <truong.nguyen3@niteco.se>
2025-06-05 16:52:32 +0700

    Initial Commit

---
+++ 03_View+Modifier/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  03_View+Modifier
+//
+//  Created by Truong Huu Nguyen on 5/6/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 03_View+Modifier/_3_View_ModifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  _3_View_ModifierApp.swift
+//  03_View+Modifier
+//
+//  Created by Truong Huu Nguyen on 5/6/25.
+//
+
+import SwiftUI
+
+@main
+struct _3_View_ModifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
