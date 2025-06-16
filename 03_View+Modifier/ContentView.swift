b86b815fff51e61bce9a2dd31e885c944a01cdbe
jeremy <nguyentruong.xyz0405@gmail.com>
2025-06-16 23:09:51 +0700

    view composition

--- 03_View+Modifier/ContentView.swift
+++ 03_View+Modifier/ContentView.swift
@@ -7,10 +7,28 @@
 
 import SwiftUI
 
+struct CapsuleText: View {
+    var text: String
+    
+    var body: some View {
+        Text(text)
+            .font(.largeTitle)
+            .padding()
+            
+            .background(.blue)
+            .clipShape(Capsule())
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello")
-        Text("World")
+        VStack(spacing:10) {
+            CapsuleText(text: "First")
+                .foregroundColor(.white)
+            CapsuleText(text: "Second")
+                .foregroundColor(.yellow)
+        }
+        
     }
 }
 
