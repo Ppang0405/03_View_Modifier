5fe20e7dc0c81593def792c873a9038d360d1a69
jeremy <nguyentruong.xyz0405@gmail.com>
2025-06-16 23:36:06 +0700

    custom view

--- 03_View+Modifier/ContentView.swift
+++ 03_View+Modifier/ContentView.swift
@@ -7,53 +7,28 @@
 
 import SwiftUI
 
-struct Watermark: ViewModifier {
-    var text: String
+struct GridStack<Content: View>: View {
+    let columns: Int
+    let rows: Int
+    @ViewBuilder let content: (Int, Int) -> Content
     
-    func body(content: Content) -> some View {
-        ZStack(alignment: .bottomTrailing) {
-            content
-            
-            Text(text)
-                .font(.caption)
-                .foregroundColor(.white)
-                .padding(5)
-                .background(.black)
-        }
-    }
-}
-
-struct Title: ViewModifier {
-    func body(content: Content) -> some View {
-        content
-            .font(.largeTitle)
-            .foregroundColor(.white)
-            .padding(.horizontal)
-            .background(.blue)
-            .clipShape(RoundedRectangle(cornerRadius: 10))
-    }
-}
-
-extension View {
-    
-    func titleStyle() -> some View {
-        self.modifier(Title())
-    }
-    
-    func watermarked(with text: String) -> some View {
-        self.modifier(Watermark(text: text))
+    var body: some View {
+                ForEach(0..<rows, id: \.self) { row in
+                    HStack {
+                        ForEach(0..<columns, id: \.self) { column in
+                            content(row, column)
+                        }
+                    }
+                }
     }
 }
 
 struct ContentView: View {
     var body: some View {
-        VStack(spacing:10) {
-            Text("Hello, world")
-                .titleStyle()
+        GridStack(columns: 4, rows: 4) { row, col in
+            Image(systemName: "\(row * 4 + col).circle")
+            Text("R\(row), C\(col)")
             
-            Color.blue
-                .frame(width: 200, height: 100)
-                .watermarked(with: "Hacking with Swift")
         }
         
     }
